e19c99f5a7846cf6bdd3bde6aaa4f33d55524493
Grunt-HMB <grunt.munro@gmail.com>
2026-05-08 09:18:00 +0200

    Add cookie management for user authentication

--- modules/auth.py
+++ modules/auth.py
@@ -1,6 +1,21 @@
 import streamlit as st
+from streamlit_cookies_manager import EncryptedCookieManager
+
 from modules.database import append_row, get_next_user_id
 from modules.utils import tournament_locked
+
+
+def get_cookie_manager():
+    cookies = EncryptedCookieManager(
+        prefix="wk2026_prono_",
+        password=st.secrets["COOKIE_PASSWORD"],
+    )
+
+    if not cookies.ready():
+        st.stop()
+
+    return cookies
+
 
 def login_user(users_df, username, password):
     username = str(username or "").strip()
@@ -18,6 +33,21 @@ def login_user(users_df, username, password):
         return None
 
     return user.iloc[0].to_dict()
+
+
+def find_user_by_id(users_df, user_id):
+    if users_df.empty:
+        return None
+
+    user = users_df[
+        users_df["user_id"].astype(str) == str(user_id)
+    ]
+
+    if user.empty:
+        return None
+
+    return user.iloc[0].to_dict()
+
 
 def register_user(users_df, username, password):
     username = str(username or "").strip()
@@ -40,16 +70,42 @@ def register_user(users_df, username, password):
         return False, "Deze naam bestaat al."
 
     new_id = get_next_user_id(users_df)
-    append_row("Users", [new_id, username, password, "FALSE"])
+
+    append_row("Users", [
+        new_id,
+        username,
+        password,
+        "FALSE",
+    ])
 
     return True, "Account aangemaakt. Je kan nu inloggen."
 
+
 def logout():
+    cookies = get_cookie_manager()
+
     if "user" in st.session_state:
         del st.session_state["user"]
+
+    if "user_id" in cookies:
+        del cookies["user_id"]
+        cookies.save()
+
     st.rerun()
 
+
 def show_sidebar(users_df):
+    cookies = get_cookie_manager()
+
+    if "user" not in st.session_state:
+        remembered_user_id = cookies.get("user_id")
+
+        if remembered_user_id:
+            remembered_user = find_user_by_id(users_df, remembered_user_id)
+
+            if remembered_user is not None:
+                st.session_state["user"] = remembered_user
+
     st.sidebar.markdown("## ⚽ WK 2026")
     st.sidebar.markdown("### Pronostiek")
 
@@ -77,23 +133,50 @@ def show_sidebar(users_df):
         name = st.text_input("Naam", key="login_name")
         pincode = st.text_input("Pincode", type="password", key="login_pin")
 
+        remember_me = st.checkbox(
+            "Ingelogd blijven",
+            value=True,
+            key="remember_me",
+        )
+
         if st.button("Inloggen", use_container_width=True):
             user = login_user(users_df, name, pincode)
+
             if user is None:
                 st.error("Naam of pincode is fout.")
             else:
                 st.session_state["user"] = user
+
+                if remember_me:
+                    cookies["user_id"] = str(user["user_id"])
+                    cookies.save()
+
                 st.rerun()
 
     with tab_register:
         if tournament_locked():
             st.warning("Registreren is afgesloten.")
 
-        name = st.text_input("Nieuwe naam", key="reg_name", disabled=tournament_locked())
-        pincode = st.text_input("Nieuwe pincode", type="password", key="reg_pin", disabled=tournament_locked())
+        name = st.text_input(
+            "Nieuwe naam",
+            key="reg_name",
+            disabled=tournament_locked(),
+        )
 
-        if st.button("Registreren", use_container_width=True, disabled=tournament_locked()):
+        pincode = st.text_input(
+            "Nieuwe pincode",
+            type="password",
+            key="reg_pin",
+            disabled=tournament_locked(),
+        )
+
+        if st.button(
+            "Registreren",
+            use_container_width=True,
+            disabled=tournament_locked(),
+        ):
             success, msg = register_user(users_df, name, pincode)
+
             if success:
                 st.success(msg)
             else:
