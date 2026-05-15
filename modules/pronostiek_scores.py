6aba87e02f3f4f9c0896d0aa75e6d6886ecbfa53
Grunt-HMB <grunt.munro@gmail.com>
2026-05-15 15:02:03 +0200

    Update pronostiek_scores.py

--- modules/pronostiek_scores.py
+++ modules/pronostiek_scores.py
@@ -8,10 +8,13 @@ def show_pronostiek_scores(user_id=None):
 
     st.divider()
 
-    col1, col2 = st.columns(2)
+    col1, col2 = st.columns(3)
 
     with col1:
         st.info("Label 1")
 
     with col2:
+        st.success("-")
+
+    with col3:
         st.success("Label 2")
