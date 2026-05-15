a3f30e90029ca8009b4f5487bac7407c489be600
Grunt-HMB <grunt.munro@gmail.com>
2026-05-15 15:08:22 +0200

    Refactor layout and improve display of scores

--- modules/pronostiek_scores.py
+++ modules/pronostiek_scores.py
@@ -2,19 +2,19 @@ import streamlit as st
 
 
 def show_pronostiek_scores(user_id=None):
+
     st.title("Pronostiek Scores")
 
-    st.write(f"Gebruiker: {user_id}")
-
-    st.divider()
-
-    col1, col2, col3 = st.columns(3)
+    col1, mid, col2 = st.columns([5, 1, 5])
 
     with col1:
         st.info("Label 1")
 
-    with col2:
-        st.success("-*-")
+    with mid:
+        st.markdown(
+            "<h2 style='text-align:center;'>-*-</h2>",
+            unsafe_allow_html=True
+        )
 
-    with col3:
+    with col2:
         st.success("Label 2")
