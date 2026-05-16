61239f1a71ade56e84c9766bb8f10d23df1756bc
Grunt-HMB <grunt.munro@gmail.com>
2026-05-16 14:49:26 +0200

    Update pronostiek_scores.py

--- modules/pronostiek_scores.py
+++ modules/pronostiek_scores.py
@@ -117,7 +117,7 @@ def show_pronostiek_scores(user_id):
         
         /* SCORE INPUTS SMALLER */
         div[data-testid="stNumberInput"] {
-            max-width: 60px;
+            max-width: 70px;
         }
         
         div[data-testid="stNumberInput"] input {
