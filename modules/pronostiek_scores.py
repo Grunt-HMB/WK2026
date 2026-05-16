8d3aa77f58c1d65d9881188445821e3cbf8a79ab
Grunt-HMB <grunt.munro@gmail.com>
2026-05-16 14:50:53 +0200

    Reduce max-width of score input to 170px

--- modules/pronostiek_scores.py
+++ modules/pronostiek_scores.py
@@ -117,7 +117,7 @@ def show_pronostiek_scores(user_id):
         
         /* SCORE INPUTS SMALLER */
         div[data-testid="stNumberInput"] {
-            max-width: 220px;
+            max-width: 170px;
         }
         
         div[data-testid="stNumberInput"] input {
